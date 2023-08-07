1d0b1d74fdfd3085d63a2afde5433371b992eead
João Barreto <Joo Barreto>
2023-08-07 20:17:06 -0300

    start: init commit

---
+++ priv/repo/migrations/20230805172223_create_catalog_books.exs
@@ -0,0 +1,36 @@
+defmodule BookStore.Repo.Migrations.CreateCatalogBooks do
+  use Ecto.Migration
+
+  def change do
+    create table(:catalog_authors) do
+      add :name, :string
+      add :origin, :string
+      add :bio, :text
+      add :photo, :string
+      add :born_at, :date
+      add :died_at, :date, null: true
+    end
+
+    create table(:catalog_books) do
+      add :title, :string
+      add :isbn, :string
+      add :synopsis, :text, null: true
+      add :recommendation, :text
+      add :published_at, :date
+      add :min_age, :integer
+    end
+
+    create table(:catalog_book_authors) do
+      add :author_id, references(:catalog_authors)
+      add :book_id, references(:catalog_books)
+    end
+
+    create table(:catalog_author_reviewers) do
+      add :book_id, references(:catalog_books)
+      add :author_id, references(:catalog_authors)
+      add :review, :text
+    end
+
+    create unique_index(:catalog_books, [:isbn])
+  end
+end
